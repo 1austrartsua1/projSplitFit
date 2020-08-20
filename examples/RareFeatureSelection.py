9846d5d215d87eb9be1c075d63d4034f0835c3c5
Jonathan Eckstein <jeckstei@business.rutgers.edu>
2020-08-19 22:12:23 -0400

    Now matches results in "for1" paper -- using logistic loss and same parameters

--- examples/RareFeatureSelection.py
+++ examples/RareFeatureSelection.py
@@ -13,15 +13,18 @@ X = sp.load_npz('data/trip_advisor/S_train.npz') # training matrix (scipy.sparse
 H = sp.load_npz('data/trip_advisor/S_A.npz')     # this matrix also in scipy.sparse format
 r = np.load('data/trip_advisor/y_train.npy')     # training labels
 
+# Convert responses of 5 to +1, and all other responses to -1
+r = 1 - 2*(r < 5)
+
 ### create projective splitting object
 projSplit = ps.ProjSplitFit()
 
 ### add data composed with linear operator
-projSplit.addData(X,r,loss=2,linearOp=H,normalize=False)
+projSplit.addData(X,r,loss='logistic',linearOp=H,normalize=False)
 
 ### first regularizer
 mu=0.5
-lam=1e-5
+lam=1e-4
 def applyG(x):
     return x[:-1]
 def applyGtranspose(v):
@@ -41,14 +44,29 @@ projSplit.addRegularizer(regObj2,linearOp=H)
 np.random.seed(1)
 
 # Set dual scaling parameter
-projSplit.setDualScaling(1e-2)
+projSplit.setDualScaling(1e-4)
 
-# Problem is run to low accuracy since it is very difficult and ill-conditioned
-projSplit.run(nblocks=10,maxIterations=None,verbose=True,
-              primalTol=1e-2,dualTol=1e-2)
+# Problem is for a limited number of iterations since it is very difficult
+projSplit.run(nblocks=10,maxIterations=20000,verbose=True,
+              primalTol=1e-2,dualTol=1e-2,keepHistory=True)
 
 # Get results
 objVal = projSplit.getObjective()
 solVector = projSplit.getSolution()
 
 print(f"Objective value = {objVal}")
+
+try:
+    import matplotlib.pyplot as plt
+except:
+    print("matplotlib appears not to be available, exiting without showing chart")
+    sys.exit(0)
+
+history = projSplit.getHistory()
+# history[1,:] is time, history[0,:] is the objective value
+plt.plot(history[1,:], history[0,:])
+plt.xlabel("Time")
+plt.ylabel("Objective Value")
+plt.show()
+
+
